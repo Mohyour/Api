9f952145037e3dc8c18f156b12ab43a8be0bbd9e
mohyour <mohyour01@gmail.com>
2016-08-22 18:05:36 +0100

    edited post file

--- post.js
+++ post.js
@@ -1,27 +1,30 @@
-// var rq = require('request')
-var http = require('http')
+var https = require("https");
+
 var options = {
-    host:'api.github.com',
+ host:'api.github.com',
     path: '/gists',
     method: 'POST',
     headers: {'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
-}
-var input = JSON.stringify({
-    "description":"Created via API",
+};
+
+var input = JSON.stringify( {
+     "description":"Created via API",
     "public":"true",
     "files":{"file1.txt":{"content":"Demo"}}
+  } );
+
+var post = https.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('Response data: ' + chunk)
+  });
 });
 
- var post_req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
-      });
-  });
+post.on('error', function(e) {
+  console.log('problem with postuest: ' + e.message);
+});
 
-    post_req.on('error', function(e) {
-        console.error('and the error is '+e);
-    });
-    post_req.write(input);
-    post_req.end();
-
+post.write(input);
+post.end();
